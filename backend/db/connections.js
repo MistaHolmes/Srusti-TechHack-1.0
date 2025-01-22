61bda2d7f55316f713ed6df949b28c1e6a3907e4
theogaditya <adityahota99@gmail.com>
2025-01-22 23:43:26 +0530

    comform

--- backend/db/connections.js
+++ backend/db/connections.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-<<<<<<< HEAD
         await mongoose.connect('xxx', {
-=======
-        await mongoose.connect('mongodb+srv://user:<password>@cluster0.vqxq4.mongodb.net/myDatabase', {
->>>>>>> 45ccceaad6bad589aa315d1e26061ec71d6008f3
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
