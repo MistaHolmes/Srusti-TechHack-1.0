93b2dc5deaadbbb225fa3c39f179a497af989d75
Aditya Hota <133393430+theogaditya@users.noreply.github.com>
2025-01-22 23:30:06 +0530

    Update connections.js

--- backend/db/connections.js
+++ backend/db/connections.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
 <<<<<<< HEAD
-        await mongoose.connect('mongodb+srv://theogadityahota:Iamspeed%402003@cluster0.rlyi7.mongodb.net/myDatabase', {
+        await mongoose.connect('xxx', {
 =======
         await mongoose.connect('mongodb+srv://user:<password>@cluster0.vqxq4.mongodb.net/myDatabase', {
 >>>>>>> 45ccceaad6bad589aa315d1e26061ec71d6008f3
